20e5e83ce179f473fff28df94690f1ff0017673e
Manu <manu.stier@me.com>
2018-04-20 14:55:30 +0200

    Initial Commit

---
+++ Conquer World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Conquer World
+//
+//  Created by Manu S. Stier on 20/4/18.
+//  Copyright © 2018 Manu S. Stier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
